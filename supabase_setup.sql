5e61487f57f7a3637d5e9e69c51375d6014afaa7
justinrhoyyy <94108229+justinrhoyyy@users.noreply.github.com>
2026-05-18 12:57:10 +0800

    update

--- supabase_setup.sql
+++ supabase_setup.sql
@@ -6,6 +6,7 @@ create table if not exists products (
   serial_number text not null,
   barcode_number text not null unique,
   barcode_image text not null,
+  delivery_date date,
   created_at timestamp with time zone not null default now()
 );
 
